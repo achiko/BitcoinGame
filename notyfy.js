82e2d49f049e453deb886cb2247c9fbf217a889e
Archil <achiko@gmail.com>
2014-04-03 12:28:18 -0700

    Delete notyfy.js

--- notyfy.js
+++
@@ -1,8 +0,0 @@
-
-
-console.log(process.argv[2]);
-
-// 1. Get transaction ID
-// 2. Call Bitcoin Client:  gettransaction method:  Get Acct
-// 3. Find Usersession via users collection 
-// 4. Send Notification to client via socket.io 
